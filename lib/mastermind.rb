febe61e7cd3a76686c428cf66f9e2bc119f98e60
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-08-24 20:31:27 +0200

    implementing mastermind_method

---
+++ lib/mastermind.rb
@@ -0,0 +1,10 @@
+require "mastermind"
+
+RSpec.describe Mastermind do
+  describe "#mastermind_board" do
+    it "accepts one correct guess but not in the right position" do
+      guesses = ["RED", "GREEN", "YELLOW", "BLUE"]
+    end
+
+  end
+end
