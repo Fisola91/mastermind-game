60e058341e1c37b50a51e95ceb953062b06f563b
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-08-26 22:42:51 +0200

    added the constant variable

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -1,31 +1,31 @@
 class Mastermind
-  attr_reader :passcode, :input, :guess_attempts, :output
-  def initialize(passcode:, input: $stdin, output: $stdout, guess_attempts: 4)
+  NUMBER_CODE = 4
+  attr_reader :passcode, :input, :chances, :output
+  def initialize(passcode:, input: $stdin, output: $stdout, chances: 4)
     @passcode = passcode
     # @correct_answers = 0
     @input = input
-    @guess_attempts = 5
+    @chances = 4
     @output = output
   end
 
-  def mastermind
+  def mastermind_board
     guess = nil
     current_attempt = 1
     correct = 0
-    while correct < guess_attempts
+    while correct < chances
       guess = input.split("\n")
 
-      if guess.length != 4
+       #check if the number of guesses are 4
+
+      if guess.length != NUMBER_CODE
         raise ArgumentError, "Number of guess not completed, try again!!"
+      else
+        guess1 = guess[0]
+        guess2 = guess[1]
+        guess3 = guess[2]
+        guess4 = guess[3]
       end
-
-      guess1 = guess[0]
-      guess2 = guess[1]
-      guess3 = guess[2]
-      guess4 = guess[3]
-
-      #check if the number of guesses are 4
-
 
       if guess1 == passcode[0]
         correct += 1
