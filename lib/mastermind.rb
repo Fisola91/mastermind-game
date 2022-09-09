16f0608c5a67aa7f7c31dac5be2f27ab607d4f6f
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-09 09:08:21 +0200

    Deleted unecessary comment in the mastermind file

--- lib/mastermind.rb
+++ lib/mastermind.rb
@@ -52,21 +52,3 @@ class Mastermind
     output.puts "You lost, ran out of turns."
   end
 end
-
-# Codemaker makes code of color peg and close
-# Codebreaker places color pegs on the empty board
-# Once the codebreaker finished, the codemaker must provide feedback
-# to the codebreaker as the how close the code is close
-# using the tiny peg, the code maker indicates what information is correct
-# For each color that corresponds(right position), use right peg
-# if the color of the peg is correct, but different position, use white peg
-# if the information is correct, no peg is used.
-# Position of the peg does indicate which is cokor is in the right order
-# Once the code breaker has matched hidden code identically, the codemaker reveal the code
-# It counts how many rows it took codebreaker to solve the code
-# The codemaker receives that mini-point------LATER
-# If the code
-
-
-# If any of the guess matches the passcode in the right position,
-#  if passcode
