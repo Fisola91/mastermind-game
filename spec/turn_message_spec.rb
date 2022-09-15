4bafa8b31f57df3924af6830103b7750dc657d6f
Fisola Adesuyi <adesuyifisola@gmail.com>
2022-09-15 13:28:04 +0200

    re-organized the structure of the rspec test for turn_message

--- spec/turn_message_spec.rb
+++ spec/turn_message_spec.rb
@@ -6,53 +6,53 @@ RSpec.fdescribe TurnMessage do
   let(:passcode) { ["RED", "GREEN", "BLUE", "YELLOW"] }
 
   describe ".for" do
-    context "all elements guessed at the exact position" do
-      it "returns congratulatory message to the player" do
-        turn = Turn.new(passcode: passcode)
-        turn_message = turn.guess(["RED", "GREEN", "BLUE", "YELLOW"])
-        expect(described_class.for(turn_message)).to eq("Congratulations!")
+    describe "exact position" do
+      context "all colors guessed at the exact position" do
+        it "returns congratulatory message to the player" do
+          turn = Turn.new(passcode: passcode)
+          turn_message = turn.guess(["RED", "GREEN", "BLUE", "YELLOW"])
+          expect(described_class.for(turn_message)).to eq("Congratulations!")
+        end
+      end
+
+      context "one color guessed at the exact position" do
+        it "returns a message (one correct guess) to the player" do
+          turn = Turn.new(passcode: passcode)
+          turn_message = turn.guess(["RED", "ORANGE", "ORANGE", "ORANGE"])
+          expect(described_class.for(turn_message)).to eq("One correct guess at the exact position")
+        end
+      end
+
+      context "two colors guessed at the exact position" do
+        it "returns a message (two correct guess) to the player" do
+          turn = Turn.new(passcode: passcode)
+          turn_message = turn.guess(["RED", "GREEN", "ORANGE", "ORANGE"])
+          expect(described_class.for(turn_message)).to eq("Two correct guess at the exact position")
+        end
+      end
+
+      context "three colors guessed at the exact position" do
+        it "returns a message (three correct guess) to the player" do
+          turn = Turn.new(passcode: passcode)
+          turn_message = turn.guess(["RED", "GREEN", "BLUE", "ORANGE"])
+          expect(described_class.for(turn_message)).to eq("Three correct guess at the exact position")
+        end
+      end
+
+      context "incorrect guess" do
+        it "returns a message (incorrect) to the player" do
+          turn = Turn.new(passcode: passcode)
+          turn_message = turn.guess(["ORANGE", "ORANGE", "ORANGE", "ORANGE"])
+          expect(described_class.for(turn_message)).to eq("Incorrect guess")
+        end
+      end
+
+      context "unimplemented guess" do
+        it "returns a message (not implemented) to the player" do
+          turn = Turn.new(passcode: passcode)
+          expect(described_class.for(turn)).to eq("Result message not implemented: #{turn.inspect}")
+        end
       end
     end
-
-    context "one color guessed at the exact position" do
-      it "returns a message (one correct guess) to the player" do
-        turn = Turn.new(passcode: passcode)
-        turn_message = turn.guess(["RED", "ORANGE", "ORANGE", "ORANGE"])
-        expect(described_class.for(turn_message)).to eq("One correct guess at the exact position")
-      end
-    end
-
-    context "two colors guessed at the exact position" do
-      it "returns a message (two correct guess) to the player" do
-        turn = Turn.new(passcode: passcode)
-        turn_message = turn.guess(["RED", "GREEN", "ORANGE", "ORANGE"])
-        expect(described_class.for(turn_message)).to eq("Two correct guess at the exact position")
-      end
-    end
-
-    context "three colors guessed at the exact position" do
-      it "returns a message (three correct guess) to the player" do
-        turn = Turn.new(passcode: passcode)
-        turn_message = turn.guess(["RED", "GREEN", "BLUE", "ORANGE"])
-        expect(described_class.for(turn_message)).to eq("Three correct guess at the exact position")
-      end
-    end
-
-    context "incorrect guess" do
-      it "returns a message (incorrect) to the player" do
-        turn = Turn.new(passcode: passcode)
-        turn_message = turn.guess(["ORANGE", "ORANGE", "ORANGE", "ORANGE"])
-        expect(described_class.for(turn_message)).to eq("Incorrect guess")
-      end
-    end
-
-    context "unimplemented guess" do
-      it "returns a message (not implemented) to the player" do
-        turn = Turn.new(passcode: passcode)
-        expect(described_class.for(turn)).to eq("Result message not implemented: #{turn.inspect}")
-      end
-    end
-
-
   end
 end
